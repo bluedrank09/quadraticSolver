19ecd12ce3bda4f293ca8f4f07a0fd260521dec1
mayababu <mayababu73@gmail.com>
2021-11-22 18:12:06 +1300

    Added comments

--- quadraticSolver.py
+++ quadraticSolver.py
@@ -1,7 +1,7 @@
 import sys
-import logging as log
+import logging as log #importing a logger in case things go wrong and I need to keep track of it
 import inspect
-import cmath
+import cmath #importing a special maths package that is able to do quadratics
 
 def printOutput(quadRoot):
     try:
@@ -14,13 +14,14 @@ def printOutput(quadRoot):
 
 def findRoots(cfxs, coeff, constant):
     try:
-        print(f"{cfxs}")
-        print(f"{coeff}")
-        print(f"{constant}")
+        #printing out the main parts of the quadratic equatoin the user has given
+        print(f"Coefficent of x sqaured is {cfxs}") 
+        print(f"Coefficient of x is {coeff}")
+        print(f"Constant is {constant}")
 
-        quadRootOne = ((coeff*-1)+(cmath.sqrt(coeff*coeff - (4*cfxs*constant))))/2*cfxs
+        quadRootOne = ((coeff*-1)+(cmath.sqrt(coeff*coeff - (4*cfxs*constant))))/2*cfxs #using the positive  quadratic formula to calculate the first root
         printOutput(f"Quadratic Root One Real : {(float(quadRootOne.real))*-1:.2f}, Quadratic Root One Imaginary : {float(quadRootOne.imag):.2f}")
-        quadRootTwo = ((coeff*-1)-(cmath.sqrt(coeff*coeff - (4*cfxs*constant))))/2*cfxs
+        quadRootTwo = ((coeff*-1)-(cmath.sqrt(coeff*coeff - (4*cfxs*constant))))/2*cfxs #using the negative quadtaric formula to calculate the second root
         printOutput(f"Quadratic Root Two Real : {(float(quadRootTwo.real))*-1:.2f}, Quadratic Root Two Imaginary : {float(quadRootTwo.imag):.2f} ")
     
     except Exception as error : 
@@ -29,6 +30,7 @@ def findRoots(cfxs, coeff, constant):
 
 def parseInput(formula):
     try: 
+        #splitting at the + signs so it can get the different parts needed from the equation
         variables = []
         variables = formula.split("+", 3)
         
@@ -36,6 +38,7 @@ def parseInput(formula):
         coeffString = variables[1]
         constantString = variables[2]
 
+        #naming each part of the equation so that the somputer can use them in the calculation
         cfxs = int(cfxsString[0])
         coeff = int(coeffString[0])
         constant = int(constantString)
@@ -48,7 +51,7 @@ def parseInput(formula):
 
 def getUserInput():
     try:
-        userInput = input(f"Give me a quadratic equation (e.g 3x^2 + 6x + 5 = 0) :")
+        userInput = input(f"Give me a quadratic equation (e.g 3x^2 + 6x + 5 = 0) :") #asking the user for an equation
         parseInput(userInput) 
 
     except Exception as error : 
